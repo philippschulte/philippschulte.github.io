ae2cb46b451ea0cbcf1ca11ed54c84b55c8941d5
philippschulte <philipp.schulte@ymail.com>
2016-11-29 23:45:51 -0800

    Add sticky navigation

--- src/js/animations.js
+++ src/js/animations.js
@@ -1,3 +1,40 @@
 $(document).ready(function() {
-  console.log('jQuery is working!!!');
+
+  /* ----- STICKY NAVIGATION -----*/
+  $('.js-section-about').waypoint(function(direction) {
+    if (direction === 'down') {
+      $('nav').addClass('sticky');
+    } else {
+      $('nav').removeClass('sticky');
+    }
+  },
+  {
+    offset: '45px'
+  });
+
+  /* ----- LINK BUTTON SCROLLING -----*/
+  $('.js-scroll-to-about').click(function() {
+    $('html, body').animate({
+      scrollTop: $('.js-section-about').offset().top
+    }, 1000);
+  });
+
+  $('.js-scroll-to-process').click(function() {
+    $('html, body').animate({
+      scrollTop: $('.js-section-process').offset().top
+    }, 1000);
+  });
+
+  $('.js-scroll-to-projects').click(function() {
+    $('html, body').animate({
+      scrollTop: $('.js-section-projects').offset().top
+    }, 1000);
+  });
+
+  $('.js-scroll-to-contact').click(function() {
+    $('html, body').animate({
+      scrollTop: $('.js-section-contact').offset().top
+    }, 1000);
+  });
+
 });
